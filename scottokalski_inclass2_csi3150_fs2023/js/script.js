3f927b913875cb5c59267b89f261b138f3a67ede
s-okal <145070456+s-okal@users.noreply.github.com>
2023-10-24 17:31:09 -0400

    added in-class assignment #2 to github pages, since it seems only one project can be hosted on pages per user.

---
+++ scottokalski_inclass2_csi3150_fs2023/js/script.js
@@ -0,0 +1,44 @@
+// SHOW elements
+function showPic1() {
+  let pic1 = document.querySelector(".pic1");
+  pic1.style.visibility = "visible";
+  let hidePic1 = document.querySelector(".hide1");
+  hidePic1.style.visibility = "visible";
+}
+
+function showPic2() {
+  let pic2 = document.querySelector(".pic2");
+  pic2.style.visibility = "visible";
+  let hidePic2 = document.querySelector(".hide2");
+  hidePic2.style.visibility = "visible";
+}
+
+function showPic3() {
+  let pic3 = document.querySelector(".pic3");
+  pic3.style.visibility = "visible";
+  let hidePic3 = document.querySelector(".hide3");
+  hidePic3.style.visibility = "visible";
+}
+
+// HIDE elements
+function hidePic1() {
+  let pic1 = document.querySelector(".pic1");
+  pic1.style.visibility = "hidden";
+  let hidePic1 = document.querySelector(".hide1");
+  hidePic1.style.visibility = "hidden";
+  hide;
+}
+
+function hidePic2() {
+  let pic2 = document.querySelector(".pic2");
+  pic2.style.visibility = "hidden";
+  let hidePic2 = document.querySelector(".hide2");
+  hidePic2.style.visibility = "hidden";
+}
+
+function hidePic3() {
+  let pic3 = document.querySelector(".pic3");
+  pic3.style.visibility = "hidden";
+  let hidePic3 = document.querySelector(".hide3");
+  hidePic3.style.visibility = "hidden";
+}
